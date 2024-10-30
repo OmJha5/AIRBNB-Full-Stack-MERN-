49e61354f20bcc3c95c7ab0ee3ea8785b21d8dc7
om <omjha003@gmail.com>
2024-10-30 18:02:03 +0530

    Showed all the listings in index.ejs file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express()
 const mongoose = require("mongoose")
 const Listing = require("./models/listing.js")
+const path = require("path")
+
+app.set("view engine" , "ejs");
+app.set("views" , path.join(__dirname , "views"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -16,22 +20,16 @@ async function main(){
     return await mongoose.connect(MONGO_URL);
 }
 
-app.get("/testListing" , async (req , res) => {
-    const newListing = new Listing({
-        title : "My name ",
-        description : "By the beach",
-        price : 200,
-        location : "Banagalore",
-        country : "India",
-    })
-    await newListing.save();
-    console.log("Listing is saved..")
-    res.send("Done the saving of the listing..")
-})
-
 app.get("/" , (req , res) => {
     res.send("Root is running..")
 })
+
+// It will show all the listings.
+app.get("/listings" , async (req , res) => {
+    const allListings = await Listing.find({});
+    res.render("Listings/index.ejs" , { allListings });
+})
+
 
 app.listen(8080 , () => {
     console.log("App is listening on port 8080");
