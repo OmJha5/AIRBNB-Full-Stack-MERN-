425a8752f9362eefedac95537d2621f0fdcd8a89
om <omjha003@gmail.com>
2024-11-21 11:01:18 +0530

    Created Review Model

--- models/listing.js
+++ models/listing.js
@@ -14,7 +14,13 @@ const listingSchema = new mongoose.Schema({
     price : Number,
     location : String,
     country : String,
-}) 
+    review : [ // One listing mai n reviews aa satke hai that is why here cardinality is 1 * n
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Review"
+        }
+    ]
+})
 
 const Listing = mongoose.model("Listing" , listingSchema)
 module.exports = Listing;
