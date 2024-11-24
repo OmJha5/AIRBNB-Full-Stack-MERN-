66ab07d88b2dd50bfac34e2ea2d96098f0d120d1
om <omjha003@gmail.com>
2024-11-24 19:05:13 +0530

    created signup page and styled it

--- router/user.js
+++ router/user.js
@@ -2,7 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/signup" , (req , res) => {
-    res.send("Form")
+    res.render("user/signup.ejs" , {signup: '/css/signup.css'})
 })
 
 module.exports = router;
