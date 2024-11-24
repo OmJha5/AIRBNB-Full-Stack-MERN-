d44a6436779385bae40ac6b2fef94b62f99c96bf
om <omjha003@gmail.com>
2024-11-24 19:39:01 +0530

    Created login page and styled it

--- router/user.js
+++ router/user.js
@@ -1,8 +1,36 @@
 const express = require("express")
 const router = express.Router()
+const User = require("../models/user.js")
 
 router.get("/signup" , (req , res) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
     res.render("user/signup.ejs" , {signup: '/css/signup.css'})
+})
+
+router.post("/signup" , async(req , res) => {
+    try{
+        const {username , email , password} = req.body;
+        console.log(username , email , password);
+        let newUser = new User({
+            email : email,
+            username : username,
+        })
+
+        await User.register(newUser , password);
+        req.flash("success" , "Enjoy Wanderlust!")
+        res.redirect("/listings")
+    }
+    catch(e){
+        req.flash("error" , e.message)
+        res.redirect("/signup")
+    }
+})
+
+router.get("/login" , (req , res) => {
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    res.render("user/login.ejs" , {signup: '/css/signup.css'})
 })
 
 module.exports = router;
