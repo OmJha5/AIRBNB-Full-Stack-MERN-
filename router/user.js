a0507923cf25a35e4d02771eab824b970ffa508b
om <omjha003@gmail.com>
2024-11-24 19:49:18 +0530

    Done with authentication of user through local services of passport for nodejs

--- router/user.js
+++ router/user.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const User = require("../models/user.js")
+const passport = require("passport")
 
 router.get("/signup" , (req , res) => {
     res.locals.success = req.flash("success");
@@ -18,8 +19,8 @@ router.post("/signup" , async(req , res) => {
         })
 
         await User.register(newUser , password);
-        req.flash("success" , "Enjoy Wanderlust!")
-        res.redirect("/listings")
+        req.flash("success" , "Signed Up Successfully , Please Login Now!")
+        res.redirect("/login")
     }
     catch(e){
         req.flash("error" , e.message)
@@ -31,6 +32,11 @@ router.get("/login" , (req , res) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.render("user/login.ejs" , {signup: '/css/signup.css'})
+})
+
+router.post("/login" , passport.authenticate("local" , {failureRedirect : "/login" , failureFlash : true}) , async(req , res) => {
+    req.flash("success" , "Welcome to Wanderlust!")
+    res.redirect("/listings")
 })
 
 module.exports = router;
